295cd07827448ea8b94cabda271c37e517eb0116
Anton Tsarenko <itantontsarenko@gmail.com>
2025-08-25 14:22:32 +0300

    feat: Create a simple Spring Boot project

---
+++ src/test/java/com/gmail/itantontsarenko/book_store_api/BookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.itantontsarenko.book_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
